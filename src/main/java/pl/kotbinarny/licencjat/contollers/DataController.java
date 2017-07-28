55dffab2106dbd56aee6949f0701d11c36a24b44
Kociński Tomasz <Tomasz.Kocinski@coi.gov.pl>
2017-07-28 12:02:41 +0200

    dodana druga tabela z id i nazwa sensora, poprawione kontrolery

--- src/main/java/pl/kotbinarny/licencjat/contollers/DataController.java
+++ src/main/java/pl/kotbinarny/licencjat/contollers/DataController.java
@@ -2,7 +2,10 @@ package pl.kotbinarny.licencjat.contollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pl.kotbinarny.licencjat.domain.Data;
 import pl.kotbinarny.licencjat.service.SensorDataServiceImpl;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/data")
@@ -15,5 +18,11 @@ public class DataController {
             @PathVariable("val") Integer value, @PathVariable("name") String name) {
         sensorDataService.addData(value,name);
         return "Data:" + value +" from dev:" + name + " is recived by server";
+    }
+    @RequestMapping
+    @ResponseBody
+    public List<Data> getAll() {
+
+        return sensorDataService.findAll();
     }
 }
--- src/main/java/pl/kotbinarny/licencjat/contollers/HomeController.java
+++ src/main/java/pl/kotbinarny/licencjat/contollers/HomeController.java
@@ -2,12 +2,15 @@ package pl.kotbinarny.licencjat.contollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.kotbinarny.licencjat.domain.Data;
 import pl.kotbinarny.licencjat.service.SensorDataServiceImpl;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -16,10 +19,22 @@ public class HomeController {
     private SensorDataServiceImpl sensorDataService;
 
 
-    @RequestMapping
-    public List<Data> home() {
-        return sensorDataService.findAll();
+    @RequestMapping("/greeting")
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetings";
     }
+    /*@GetMapping("/data")
+    public String home(Model model){
+        Map<String,Long> mapa = new HashMap<>();
+        for (Data i : sensorDataService.findAll()){
+            mapa.put(i.getData(),i.getValue().longValue());
+        }
+        model.addAttribute("mapa",mapa);
+        return "index";
+    }*/
+
+
     @RequestMapping("/dodajprzy")
     public String dodajPrzykladoweRekordy() {
         sensorDataService.addData(2,"test");
